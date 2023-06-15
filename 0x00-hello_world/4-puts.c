1dfb64f4e687bb155bc233b7ee121c7aec412d14
Bettieno <nelsonbett41@gmail.com>
2023-06-15 12:04:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main .Programming in c
+ * main . Programming in c
  * return: #(NELSON)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 return (0);
 }
