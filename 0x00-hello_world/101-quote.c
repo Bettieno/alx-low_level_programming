16b936e585d947a7d50c3f60c6bfd58672563af0
Bettieno <nelsonbett41@gmail.com>
2023-06-15 13:00:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unixstd.h>
+/**
+ * main . Simple operating system
+ * return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar\n", 2015-10-19);
+return(1);
+}
