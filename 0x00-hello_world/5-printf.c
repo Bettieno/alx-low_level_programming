96cf41b51ab5d103e33f402f7f5d80c882825f74
Bettieno <nelsonbett41@gmail.com>
2023-06-15 07:06:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - my first printf
+ * return: # (success)
+ */
+int main(void)
+{
+	prIintf( "with proper grammar, but the outcome is a piece of art\n")
+	return(0);
+}
