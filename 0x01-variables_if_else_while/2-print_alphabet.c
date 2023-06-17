41e081176d184e914d8d6f88befcea9d6521ee90
Bettieno <nelsonbett41@gmail.com>
2023-06-17 06:05:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Alphabet.
+ *
+ * Return: Always # (Success)
+ */
+int main(void)
+{
+	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar (alp[a]);
+	}
+	putchar("/n");
+	return (0);
+}
